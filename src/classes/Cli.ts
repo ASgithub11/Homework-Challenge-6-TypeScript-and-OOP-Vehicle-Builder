be8cff544049cf9c81df98a179adcc0dc04b8eef
Ash <aishasiddiqa151@gmail.com>
2024-08-02 10:47:12 -0700

    Add statements to perform wheelie action for motorbike

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -400,6 +400,13 @@ class Cli {
           }
         }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
+        else if (answers.action === 'Wheelie') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin && this.vehicles[i] instanceof Motorbike) {
+              (this.vehicles[i] as Motorbike).wheelie();
+            }
+          }
+        }  
         else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
